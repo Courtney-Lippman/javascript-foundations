cbdb46bb9a8cac90667c55daa0cbdc6bec44c2c2
Courtney Lippman <courtney.lippman@colorado.edu>
2022-08-31 07:42:28 -0600

    Feat: add passing features for first 6 tests. WIP test 6.

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,16 @@
+class Dragon {
+  constructor(name, rider, isHungry = true) {
+    this.name = name;
+    this.rider = rider
+    this.hungry = isHungry;
+  }
+  greet() {
+    return `Hi, ${this.rider}!`;
+  }
+  eat() {
 
+  }
+}
+
+
+module.exports = Dragon;
