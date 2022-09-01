7e5251ed059253b0489e55720a724bc1e1945358
Courtney Lippman <courtney.lippman@colorado.edu>
2022-09-01 07:32:12 -0600

    fix: fix eat method to pass the last test.

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,14 +1,24 @@
+var meals = 0;
+
+
 class Dragon {
   constructor(name, rider, isHungry = true) {
     this.name = name;
     this.rider = rider
     this.hungry = isHungry;
   }
+
   greet() {
     return `Hi, ${this.rider}!`;
   }
-  eat() {
 
+  eat() {
+  meals = meals + 1
+  console.log(meals)
+    if (meals === 3) {
+    return this.hungry = false;
+    }
+    return meals
   }
 }
 
