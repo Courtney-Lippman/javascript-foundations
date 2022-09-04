9baaf1af3036035e724dae55b3b79586da25bfcd
Courtney Lippman <courtney.lippman@colorado.edu>
2022-09-04 07:23:11 -0600

    Add functionality for the celebrateBirthday function and the getRing function which passes all of the tests.

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,13 +1,28 @@
 class Hobbit {
-  constructor (name, age = 0, adult = false) {
+  constructor (name, age = 0, adult = false, old = false, hasRing = false) {
     this.name = name.name
     this.age = age
-    this.adult = false
+    this.adult = adult
+    this.old = old
+    this.hasRing = hasRing
   }
+
   celebrateBirthday () {
     this.age += 1
     if (this.age > 32) {
       this.adult = true
+      if (this.age > 100) {
+        this.old = true
+      }
+    }
+  }
+
+  getRing () {
+    if (this.name === 'Frodo' ) {
+      this.hasRing = true
+      return 'Here is the ring!'
+    } else {
+      return "You can't have it!"
     }
   }
 }
