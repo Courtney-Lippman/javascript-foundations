ec0ae065789352295c4be0044cb487a6444a0927
Courtney Lippman <courtney.lippman@colorado.edu>
2022-09-04 08:07:42 -0600

    Add booty property with a default of 0.

--- mythical-creatures/exercises/pirate.js
+++ mythical-creatures/exercises/pirate.js
@@ -1,8 +1,9 @@
 class Pirate {
-  constructor (name, job = 'scallywag', cursed = false) {
+  constructor (name, job = 'scallywag', cursed = false, booty = 0) {
     this.name = name
     this.job = job
     this.cursed = cursed
+    this.booty = booty
   }
 
   robShip () {
