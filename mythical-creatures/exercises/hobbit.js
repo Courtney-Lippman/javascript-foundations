69ab0c7c990f6e441b95868f28250c9d77f908de
Courtney Lippman <courtney.lippman@colorado.edu>
2022-09-04 07:04:39 -0600

    Adds functionality that passes all of the first 6 tests.

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,20 +1,15 @@
 class Hobbit {
-  constructor(name, age = 0) {
-  this.name = name.name
-  this.age = age
+  constructor (name, age = 0, adult = false) {
+    this.name = name.name
+    this.age = age
+    this.adult = false
   }
-celebrateBirthday() {
-  this.age += 1;
+  celebrateBirthday () {
+    this.age += 1
+    if (this.age > 32) {
+      this.adult = true
+    }
+  }
 }
-}
 
-
-
-
-
-
-
-
-
-
-module.exports = Hobbit;
+module.exports = Hobbit
