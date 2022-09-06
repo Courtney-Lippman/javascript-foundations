0695e292558d781d76716cd2c507c391a4957fc0
Courtney Lippman <courtney.lippman@colorado.edu>
2022-09-06 08:21:18 -0600

    Add name as a property.

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,6 +1,10 @@
 var Person = require('./person')
 var Statue = require('./statue')
 
-class Medusa {}
+class Medusa {
+  constructor (name) {
+    this.name = name
+  }
+}
 
 module.exports = Medusa
