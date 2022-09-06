b0ea3e2e015cd14a370d3a4132e92dd6e8bfc322
Courtney Lippman <courtney.lippman@colorado.edu>
2022-09-06 07:50:46 -0600

    Add Medusa class.

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,7 @@
-var Person = require('./person');
-var Statue = require('./statue');
+var Person = require('./person')
+var Statue = require('./statue')
+
+class Medusa {
+}
+
+module.exports = Medusa
